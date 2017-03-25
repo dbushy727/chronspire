adc7aed06dc94513340bc75dadd3d134505a9297
Daniel Bushkanets <daniel.bushkanets@ppfa.org>
2017-03-25 19:46:14 -0400

    navs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,116 +1,120 @@
 
     @include('header')
-    <!-- Header -->
-    <header>
-        <div class="container-fluid">
-            <div class="row">
-                <img class="splash_image" src="/img/main.jpg" alt="Basilisk">
+    @include('sub-nav')
+
+    <div class="page-wrapper">
+        <!-- Header -->
+        <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <img class="splash_image" src="/img/main.jpg" alt="Basilisk">
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
 
-    <section id="collection">
-        @include('portfolio')
-    </section>
+        <section id="collection">
+            @include('portfolio')
+        </section>
 
-    <section class="banner" id="first-banner">
-        <div class="container">
-            <div class="row"></div>
-        </div>
-    </section>
+        <section class="banner" id="first-banner">
+            <div class="container">
+                <div class="row"></div>
+            </div>
+        </section>
 
-    <section id="features">
-        @include('features')
-    </section>
+        <section id="features">
+            @include('features')
+        </section>
 
-    <section class="banner" id="second-banner">
-        <div class="container">
-            <div class="row"></div>
-        </div>
-    </section>
+        <section class="banner" id="second-banner">
+            <div class="container">
+                <div class="row"></div>
+            </div>
+        </section>
 
-    <section id="box">
-        @include('box')
-    </section>
+        <section id="box">
+            @include('box')
+        </section>
 
-    <section class="banner" id="third-banner">
-        <div class="container">
-            <div class="row"></div>
-        </div>
-    </section>
+        <section class="banner" id="third-banner">
+            <div class="container">
+                <div class="row"></div>
+            </div>
+        </section>
 
-    <section id="gallery">
-        @include('gallery')
-    </section>
+        <section id="gallery">
+            @include('gallery')
+        </section>
 
-    <section id="about">
-        @include('about_us')
-    </section>
-
-    <div class="separator"></div>
-
-    <!-- Contact Section -->
-    <section id="subscribe">
-        @include('subscribe')
+        <section id="about">
+            @include('about_us')
+        </section>
 
         <div class="separator"></div>
 
-    </section>
+        <!-- Contact Section -->
+        <section id="subscribe">
+            @include('subscribe')
 
-    <!-- Footer -->
-    <footer class="text-center">
-        <div class="footer-below">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        Copyright &copy; Chronspire {{ Carbon\Carbon::now()->format('Y')}}
+            <div class="separator"></div>
+
+        </section>
+
+        <!-- Footer -->
+        <footer class="text-center">
+            <div class="footer-below">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            Copyright &copy; Chronspire {{ Carbon\Carbon::now()->format('Y')}}
+                        </div>
                     </div>
                 </div>
             </div>
+        </footer>
+
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+        <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+            <a class="btn btn-primary" href="#page-top">
+                <i class="fa fa-chevron-up"></i>
+            </a>
         </div>
-    </footer>
 
-    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
-    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
-        <a class="btn btn-primary" href="#page-top">
-            <i class="fa fa-chevron-up"></i>
-        </a>
+        <div id="subscribeModal" class="modal fade" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-body">
+                <h3 class="text-center">Subscribe to Our Mailing List</h3>
+
+                <form name="subscribeModalForm" id="subscribeModalForm" novalidate>
+                    {{ csrf_field() }}
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" placeholder="Name" id="modal-name" required data-validation-required-message="Please enter your name.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <div class="row control-group">
+                        <div class="form-group col-xs-12 floating-label-form-group controls">
+                            <label>Email Address</label>
+                            <input type="email" name="email" class="form-control" placeholder="Email Address" id="modal-email" required data-validation-required-message="Please enter your email address.">
+                            <p class="help-block text-danger"></p>
+                        </div>
+                    </div>
+                    <br>
+                    <div id="success"></div>
+                    <div class="row">
+                        <div class="form-group col-xs-12 text-center">
+                            <button id="saveSubscriberModalButton" class="btn btn-success btn-lg">Subscribe</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="text-center"><a href="#" id="no-thanks">No Thanks</a></div>
+
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
     </div>
-
-    <div id="subscribeModal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-body">
-            <h3 class="text-center">Subscribe to Our Mailing List</h3>
-
-            <form name="subscribeModalForm" id="subscribeModalForm" novalidate>
-                {{ csrf_field() }}
-                <div class="row control-group">
-                    <div class="form-group col-xs-12 floating-label-form-group controls">
-                        <label>Name</label>
-                        <input type="text" name="name" class="form-control" placeholder="Name" id="modal-name" required data-validation-required-message="Please enter your name.">
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <div class="row control-group">
-                    <div class="form-group col-xs-12 floating-label-form-group controls">
-                        <label>Email Address</label>
-                        <input type="email" name="email" class="form-control" placeholder="Email Address" id="modal-email" required data-validation-required-message="Please enter your email address.">
-                        <p class="help-block text-danger"></p>
-                    </div>
-                </div>
-                <br>
-                <div id="success"></div>
-                <div class="row">
-                    <div class="form-group col-xs-12 text-center">
-                        <button id="saveSubscriberModalButton" class="btn btn-success btn-lg">Subscribe</button>
-                    </div>
-                </div>
-            </form>
-            <div class="text-center"><a href="#" id="no-thanks">No Thanks</a></div>
-
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
     @include('footer')
---
+++ resources/views/top-nav.blade.php
@@ -0,0 +1,37 @@
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom affix">
+    <div class="container">
+
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span><i class="fa fa-bars"></i>
+            </button>
+            <a href="/"><img id="logo" src="/img/Chronspire-Logo-white.png" alt="logo"></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/mission">Mission</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/basilisk">The Basilisk</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/founder">About The Founder</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/about-us">About Us</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
