fa834caa67a1b72525ca758f686ef3175b21a7fb
Daniel Bushkanets <daniel.bushkanets@ppfa.org>
2017-03-25 18:54:49 -0400

    cookie subscriber modal

--- resources/assets/js/freelancer.js
+++ resources/assets/js/freelancer.js
@@ -64,8 +64,36 @@
         $('#subscribeModal').modal('hide');
     });
 
-    // launch subscribe modal
-    // $('#subscribeModal').modal();
+
+    function createCookie(name,value,hours) {
+        var expires = "";
+        if (hours) {
+            var date = new Date();
+            date.setTime(date.getTime() + (hours*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name,"",-1);
+    }
+
+    if (readCookie('chronspire') !== 'shownSignup') {
+        $('#subscribeModal').modal();
+        createCookie('chronspire', 'shownSignup', 1);
+    }
 
     // Floating label headings for the contact form
     $(function() {
