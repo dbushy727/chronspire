35f945a896f9c1c921706024e6d2b1ce17e34312
Daniel Bushkanets <daniel.bushkanets@ppfa.org>
2017-03-26 12:50:18 -0400

    modal close

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -32,6 +32,11 @@
 <div id="showSupportingImageModal" class="modal fade" tabindex="-1" role="dialog">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
+      <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+      </div>
       <div class="modal-body">
         <img src="#" class="img-responsive modal-image">
       </div>
