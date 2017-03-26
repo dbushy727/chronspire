544550003923d790824baeea15959d41229a197e
Daniel Bushkanets <daniel.bushkanets@ppfa.org>
2017-03-26 12:02:06 -0400

    scroll spy

--- resources/assets/js/freelancer.js
+++ resources/assets/js/freelancer.js
@@ -7,16 +7,16 @@
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top - 120)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
-    // $('body').scrollspy({
-    //     target: '.navbar-fixed-top',
-    //     offset: 51
-    // });
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 121
+    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){
